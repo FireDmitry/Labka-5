4679f63a9fa7fc365afc7f1a1cf9d456d0e623d6
FireDmitry <114346686+FireDmitry@users.noreply.github.com>
2023-01-12 04:32:55 +0200

    Update and rename Labkasa 5/Labkasa 5.cpp to Лабкаса 5/Labkasa 5.cpp

---
+++ Лабкаса 5/Labkasa 5.cpp
@@ -0,0 +1,17 @@
+﻿// Labka 5 
+#include <iostream>
+using namespace std;
+int main()
+{
+    int* pa = new int;
+	*pa = 7;
+	int* ptx = pa;
+
+	cout << *pa << endl;
+	cout << pa << endl;
+
+	delete pa;
+	pa = 0;
+	cout << pa << endl << *ptx << endl;
+	return 0;
+}
